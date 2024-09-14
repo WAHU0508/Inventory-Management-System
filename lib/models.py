6e8603b5cd821e3abd3ef407bfbaec88a888e3c9
WAHU0508 <sharongikenye@gmail.com>
2024-09-14 17:34:07 +0300

    Create models

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import ForeignKey, Table, Column, Integer, String, MetaData, create_engine
+from sqlalchemy import ForeignKey, Table, Column, Integer, String, MetaData, func, DateTime, create_engine
+from sqlalchemy.types import DECIMAL
 from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -12,3 +13,31 @@ Base = declarative_base(metadata=metadata)
 engine = create_engine('sqlite:///inventories.db')
 Session = sessionmaker(bind=engine)
 session = Session()
+
+class Item(Base):
+    __tablename__ = 'items'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    price = Column(DECIMAL(10, 2))
+
+    category_id = Column(Integer(), ForeignKey('categories.id'))
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+class StockLevel(Base):
+    __tablename__ = 'stocklevels'
+
+    id = Column(Integer(), primary_key=True)
+    item_id = Column(Integer(), ForeignKey('items.id'))
+    quantity = Column(Integer())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
+    
+
+# class Supplier(Base):
+#     pass
